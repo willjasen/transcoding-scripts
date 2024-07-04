4f8398c1db930139ff3edc1c3b717a66f83c0f7c
willjasen <brandon@willjasen.com>
2024-07-04 01:39:38 -0400

    initial commit of transcode-to-av1.sh

---
+++ transcode-to-av1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Input file (passed by Plex DVR)
+INPUT_FILE="$1"
+
+# Output file (same name, but in AV1 format)
+OUTPUT_FILE="${INPUT_FILE%.*}.h265.mkv"
+
+# Convert video to AV1 using ffmpeg
+ffmpeg -i "$INPUT_FILE" -c:v libaom-av1 -crf 30 -b:v 0 -strict experimental -map_metadata 0 -c:a copy "$OUTPUT_FILE"
+
+# Check if the conversion was successful
+if [ $? -eq 0 ]; then
+    echo "Conversion successful!"
+#    echo "Conversion successful. Deleting original file."
+#    rm "$INPUT_FILE"
+else
+    echo "Conversion failed... keeping original file."
+fi
+
+exit 0
