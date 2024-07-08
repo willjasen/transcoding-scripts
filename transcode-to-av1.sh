d76c8594eaec2e048ac1f8f5f6029500cd738b77
willjasen <brandon@willjasen.com>
2024-07-08 09:35:48 -0400

    change file extension

--- transcode-to-av1.sh
+++ transcode-to-av1.sh
@@ -2,9 +2,10 @@
 
 # Input file (passed by Plex DVR)
 INPUT_FILE="$1"
+echo "Input file is: ${INPUT_FILE}";
 
 # Output file (same name, but in AV1 format)
-OUTPUT_FILE="${INPUT_FILE%.*}.h265.mkv"
+OUTPUT_FILE="${INPUT_FILE%.*}.av1.mkv"
 
 # Convert video to AV1 using ffmpeg
 ffmpeg -i "$INPUT_FILE" -c:v libaom-av1 -crf 30 -b:v 0 -strict experimental -map_metadata 0 -c:a copy "$OUTPUT_FILE"
